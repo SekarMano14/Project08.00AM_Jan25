ee6069c65d0254476dd6c81b5e2ed310207484db
SekarMano14 <sekarmano46@gmail.com>
2025-02-04 08:44:56 +0530

    Kumar's new code

--- src/test/java/org/test/Test.java
+++ src/test/java/org/test/Test.java
@@ -3,6 +3,9 @@ package org.test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Test1");
+		System.out.println("Test5");
+		System.out.println("Test6");
+		System.out.println("Test7");
 	}
 
 
